ad547d2090b1d569e50b1f995d3a963633000d5f
ilian572 <ilian572@gmail.com>
2021-01-01 17:57:56 +0100

    adding cookies

--- assets/js/js-izilang.js
+++ assets/js/js-izilang.js
@@ -1,8 +1,47 @@
+/*
+  Cookies
+*/
+
+function setCookie(name, value, days) {
+  var expires;
+
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
+  } else {
+      expires = "";
+  }
+  document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = encodeURIComponent(name) + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ')
+          c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0)
+          return decodeURIComponent(c.substring(nameEQ.length, c.length));
+  }
+  return null;
+}
+
+function delCookie(name) {
+  createCookie(name, "", -1);
+}
+
+/*
+  JS IziLang
+*/
+
 //need jQuery
 
 const ildefault="fr"
 
 function changeLang(lang) {
+  setCookie("lang", lang, 365)
   if ($(".ilselector") !== undefined) $(".ilselector").val(lang)
   $("*[ild]").each( (i, el) => {
     if  ($(el).attr("ilt") !== undefined) $(el).attr($(el).attr("ilt"), LANGS[lang][$(el).attr("ild")])
@@ -14,7 +53,11 @@ function changeLang(lang) {
 
 $( document ).ready( () => {
   $(".ilselector").attr("onchange", "changeLang(this.value)")
-
-  //remove this if you don't want lang initialisation
-  changeLang(ildefault)
+  
+  if (getCookie("lang") === null) {
+    changeLang(ildefault)
+    setCookie("lang", ildefault, 365)
+  } else {
+    changeLang(getCookie("lang"))
+  }
 });
